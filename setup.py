6201b21903ccb67956fc8055f088162b5e428c3d
dhana babu <dhana36.m@gmail.com>
2017-05-23 23:08:14 +0530

    config added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='accessdb',
+    packages=['accessdb'],
+    version='0.1',
+    description='A random test lib',
+    author='Peter Downs',
+    author_email='peterldowns@gmail.com',
+    url='https://github.com/dhanababum/accessdb',
+    download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+    keywords=['python', 'accessdb', 'text'],
+    classifiers=[],
+)
