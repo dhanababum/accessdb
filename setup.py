7a37767bb3f0feabe62c1629a245e4c765d83d3f
dhana babu <dhana36.m@gmail.com>
2017-05-24 00:31:26 +0530

    git pypi going to add

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from distutils.core import setup
+from __future__ import absolute_import
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
+
+with open('README.md') as fp:
+    readme = fp.read()
 
 setup(
     name='accessdb',
     packages=['accessdb'],
-    version='0.2',
+    version='0.1',
     description='Fast way to create Access Database',
+    long_description=readme,
     author='Dhana Babu',
     author_email='dhana36.m@gmail.com',
     url='https://github.com/dhanababum/accessdb',
