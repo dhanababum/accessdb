55ca802285c49edba55c928c1ce8f1cae2e75f7a
Dhana Babu <dhana36.m@gmail.com>
2019-08-27 18:17:16 -0500

    Fixed the README file read issue

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from __future__ import absolute_import
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+PACKAGE_PATH = os.path.abspath(os.path.dirname(__file__))
 
-with open('README.md') as fp:
+with open(os.path.join(PACKAGE_PATH, 'README.md'), encoding='utf-8') as fp:
     readme = fp.read()
 
 setup(
     name='accessdb',
     packages=['accessdb'],
-    version='0.1',
+    version='0.0.1',
     description='Fast way to create Access Database',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Dhana Babu',
     author_email='dhana36.m@gmail.com',
     url='https://github.com/dhanababum/accessdb',
